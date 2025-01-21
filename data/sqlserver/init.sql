e6b89f1c1ac88e74f60e28eec4a45a8c3acc3a1b
rogeriomm <rogermm@gmail.com>
2025-01-21 07:57:14 -0300

    Update SQL Server scripts

--- data/sqlserver/init.sql
+++ data/sqlserver/init.sql
@@ -1,4 +1,7 @@
+-- https://learn.microsoft.com/en-us/sql/tools/sqlcmd/sqlcmd-utility?view=sql-server-ver16&tabs=go%2Cwindows&pivots=cs1-bash
+
 select @@VERSION;
 
 -- testdb
+:r /data/db/testdb/init.sql
 :r /data/db/testdb/testdb.sql
